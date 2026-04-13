2707ee310d0a5e0f684835487aba943c9b237a4f
wesley <wesleycostaee@gmail.com>
2026-04-13 12:47:44 +0200

    add js

---
+++ js/site_contact.js
@@ -0,0 +1,6 @@
+window.SITE_CONTACT = Object.freeze({
+  // Edit these values manually before publishing.
+  name: "Wesley Costa",
+  email: "w.da.silva.costa@pl.hanze.nl",
+  lastEdition: "April 13, 2026"
+});
